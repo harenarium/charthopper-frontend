d9eed2132b45dd015dd5e6c9955c6420b2cdb75a
harenarium <22226614+harenarium@users.noreply.github.com>
2018-04-12 20:09:24 -0400

    broken push

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ChartComponent from './components/ChartComponent.js'
+import TestingThings from './components/testingThings.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ChartComponent />
+        <ChartComponent />
+        <TestingThings text='Write the docs' />
+      </div>
+    );
+  }
+}
+
+export default App;
